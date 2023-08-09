d040bee439aef250366197df7139d8a679f53e4a
eugenehan94 <eugene.han94@gmail.com>
2023-08-09 15:49:13 -0400

    refactored personal hero component

--- src/app/components/personal/hero/hero.component.ts
+++ src/app/components/personal/hero/hero.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { HeroService } from './hero.service';
+import { HeroBannerOptionsType } from '../../_shared/interfaces';
 
 @Component({
   selector: 'app-personal-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css']
+  styleUrls: ['./hero.component.css'],
+  // https://stackoverflow.com/questions/44210786/style-not-working-for-innerhtml-in-angular
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeroComponent {
+  // Randomly select one of the objects from the array of objects heroBannerOptions
+  heroBannerOptions: HeroBannerOptionsType[] = this.service.heroBannerOptions;
+  randomIndex: number = Math.floor(
+    Math.random() * this.heroBannerOptions.length
+  );
+  selectedBanner: HeroBannerOptionsType =
+    this.heroBannerOptions[this.randomIndex];
 
+  @Input() currentScreenSize?: string;
+  
+  constructor(private service: HeroService) {}
 }
