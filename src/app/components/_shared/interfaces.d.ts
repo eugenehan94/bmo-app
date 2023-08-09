dc5e978e94e857bcfee21d92b2c6b7f063bf1d7f
eugenehan94 <eugene.han94@gmail.com>
2023-08-09 15:46:32 -0400

    added global interface file for typescript

---
+++ src/app/components/_shared/interfaces.d.ts
@@ -0,0 +1,90 @@
+// footer
+export interface CidcDataType {
+  href: string;
+  ariaLabel: string;
+  src: string;
+  alt: string;
+}
+
+interface LinksType {
+  name: string;
+  src: string;
+  ariaLabel: string;
+}
+export interface FooterCategoryLinksType {
+  title: string;
+  links: LinksType[];
+}
+
+export interface FooterSupportLinksType {
+  icon: string;
+  title: string;
+  src: string;
+  ariaLabel: string;
+}
+
+export interface SocialMediaDataType {
+  icon: string;
+  ariaLabel: string;
+  href: string;
+  alt: string;
+}
+
+export interface AboutBmoDataType {
+  href: string;
+  ariaLabel: string;
+  text: string;
+}
+
+export interface DownloadAppDataType {
+  href: string;
+  ariaLabel: string;
+  imageSrc: string;
+  imageAlt: string;
+}
+
+//personal hero
+export interface HeroBannerOptionsType {
+  src: string;
+  alt: string;
+  title: string;
+  description: string;
+  linkHref: string;
+  linkAriaLabel: string;
+  linkName: string;
+}
+
+//personal navbar
+export interface SiteOptionsType {
+  title: string;
+}
+export interface NavigationOptionsType {
+  title: string;
+}
+
+//personal products-tabs
+export interface TabsType {
+  label: string;
+  cardData: CardDataType[];
+}
+interface CardDataType {
+  image?: string;
+  svg?: string;
+  imageAlt?: string;
+  svgAlt?: string;
+  title: string;
+  description: string;
+  src: string;
+  srcTitle: string;
+}
+
+//personal where-to-begin
+export interface CardContentType {
+  icon: string;
+  iconAlt: string;
+  title: string;
+  description: string;
+  linkTitle: string;
+  linkSrc: string;
+  linkAriaLabel: string;
+}
