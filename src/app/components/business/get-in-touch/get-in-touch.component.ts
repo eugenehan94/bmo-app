bdf539c4cf166b52982d39edae41de2af9315d98
eugenehan94 <eugene.han94@gmail.com>
2023-09-19 22:11:24 -0400

    get-in-touch component added toggle for aria-selected

--- src/app/components/business/get-in-touch/get-in-touch.component.ts
+++ src/app/components/business/get-in-touch/get-in-touch.component.ts
@@ -20,20 +20,45 @@ export class BusinessGetInTouchComponent implements OnInit {
 
   handleButtonClick(i: number): void {
     const button = this.buttonsList._results;
+    console.log('button: ', button);
     button.forEach(() => {
       if (i === this.selectedButton) {
         document
           .getElementById(`${'tabButton-' + i}`)
           ?.classList.toggle('active');
+
+        let tabindex = document
+          .getElementById(`${'tabButton-' + i}`)
+          ?.getAttribute('tabIndex');
+        if (tabindex === '-1') {
+          document
+            .getElementById(`${'tabButton-' + i}`)
+            ?.setAttribute('tabIndex', '0');
+        } else {
+          document
+            .getElementById(`${'tabButton-' + i}`)
+            ?.setAttribute('tabIndex', '-1');
+        }
       } else if (i !== this.selectedButton) {
         document
           .getElementById(`${'tabButton-' + i}`)
           ?.classList.toggle('active');
+        document
+          .getElementById(`${'tabButton-' + i}`)
+          ?.setAttribute('tabIndex', '0');
+        document
+          .getElementById(`${'tabButton-' + i}`)
+          ?.setAttribute('aria-selected', 'true');
         // Remove the active class from previously selected button
         document
           .getElementById(`${'tabButton-' + this.selectedButton}`)
           ?.classList.remove('active');
-
+        document
+          .getElementById(`${'tabButton-' + this.selectedButton}`)
+          ?.setAttribute('tabIndex', '-1');
+        document
+          .getElementById(`${'tabButton-' + this.selectedButton}`)
+          ?.setAttribute('aria-selected', 'false');
         this.selectedButton = i;
       }
     });
