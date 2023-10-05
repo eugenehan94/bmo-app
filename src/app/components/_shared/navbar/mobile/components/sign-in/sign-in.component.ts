e90e8fcc7fecb77a36183e7cdba129f18368657e
eugenehan94 <eugene.han94@gmail.com>
2023-10-05 14:10:07 -0400

    changed sign-in menu mobile component to follow WCAG tab flow

--- src/app/components/_shared/navbar/mobile/components/sign-in/sign-in.component.ts
+++ src/app/components/_shared/navbar/mobile/components/sign-in/sign-in.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NavbarService } from 'src/app/components/_shared/navbar/navbar.service';
 import { SignInMenuOptionsType } from 'src/app/components/_shared/interfaces';
 
 // ngrx
 import { Store } from '@ngrx/store';
-import { setIsMobileMenuOpen, setIsMobileSignInMenuOpen } from 'src/app/store/app/actions/app.actions';
+import {
+  setIsMobileMenuOpen,
+  setIsMobileSignInMenuOpen,
+} from 'src/app/store/app/actions/app.actions';
 @Component({
   selector: 'app-mobile-sign-in',
   templateUrl: './sign-in.component.html',
@@ -18,15 +21,35 @@ export class SignInComponent implements OnInit {
   isMobileSignInMenuOpen?: boolean;
   signInMenuOptions: SignInMenuOptionsType[] =
     this.navbarService.signInMenuOptions;
+  @ViewChild('MobileSigninMenuContainer') mobileSigninMenuContainer: any;
 
   ngOnInit(): void {
     this.store.select('navbarReducer').subscribe((res) => {
       this.isMobileSignInMenuOpen = res.isMobileSignInMenuOpen;
-    })
+    });
   }
   handleSignInMenuClick(): void {
     // Closes the Menu option if it is open
     this.store.dispatch(setIsMobileMenuOpen({ isOpen: false }));
-    this.store.dispatch(setIsMobileSignInMenuOpen({isOpen: !this.isMobileSignInMenuOpen}))
+   
+    const mobileSigninMenu = this.mobileSigninMenuContainer.nativeElement;
+    mobileSigninMenu.classList.add('open');
+    // https://codepen.io/kevinpowell/pen/QWaBeGm
+    if (this.isMobileSignInMenuOpen) {
+      mobileSigninMenu.classList.add('closing');
+      // @NOTE: Waits for closing animation to finish - then component restarts
+      // to original CSS with display: none. Thus, animation and tab-able protected.
+      mobileSigninMenu.addEventListener(
+        'animationend',
+        () => {
+          mobileSigninMenu.classList.remove('closing');
+          mobileSigninMenu.classList.remove('open');
+          this.store.dispatch(setIsMobileSignInMenuOpen({ isOpen: false }));
+        },
+        { once: true }
+      );
+    } else {
+      this.store.dispatch(setIsMobileSignInMenuOpen({ isOpen: true }));
+    }
   }
 }
