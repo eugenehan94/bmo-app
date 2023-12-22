1c73c72cc2225f89468e5abdc32a632f95541eef
eugenehan94 <eugene.han94@gmail.com>
2023-12-22 15:01:40 -0500

    completed personal banking accounts featured products component

--- src/app/components/personal/bank-accounts/featured-products/featured-products.component.ts
+++ src/app/components/personal/bank-accounts/featured-products/featured-products.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+//ngrx
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-personal-bank-accounts-featured-products-component',
   templateUrl: './featured-products.component.html',
-  styleUrls: ['./featured-products.component.css']
+  styleUrls: ['./featured-products.component.css'],
 })
-export class PersonalBankAccountsFeaturedProductsComponent {
+export class PersonalBankAccountsFeaturedProductsComponent implements OnInit {
+  constructor(private store: Store<any>) {}
+  showOverlay?: boolean;
+  currentScreenSize?: any;
+  @ViewChild('cardOneOverlay') cardOneOverlay: any;
+  ngOnInit(): void {
+    this.store.select('screenSizeReducer').subscribe((res) => {
+      this.currentScreenSize = res.currentScreenSize;
+    });
+  }
 
+  setOverlay(value: boolean) {
+    this.showOverlay = value;
+  }
+  clickedOverlay(event: any): void {
+    // closes dialog when click on overlay (outside modal) when its open
+    if (event.target === this.cardOneOverlay.nativeElement) {
+      this.showOverlay = false;
+    }
+  }
 }
