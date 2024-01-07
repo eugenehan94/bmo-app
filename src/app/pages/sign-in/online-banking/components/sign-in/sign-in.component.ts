b768f3a4f57c2611fbfd7774cfcaf4e6be09ec9d
eugenehan94 <eugene.han94@gmail.com>
2024-01-06 19:08:21 -0500

    working on sign on page components

--- src/app/pages/sign-in/online-banking/components/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/online-banking/components/sign-in/sign-in.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//ngrx
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  constructor(private store: Store<any>) {}
 
+  currentScreenSize?: string;
+
+  ngOnInit(): void {
+    this.store.select('screenSizeReducer').subscribe((res) => {
+      this.currentScreenSize = res.currentScreenSize;
+      console.log(this.currentScreenSize)
+    });
+  }
 }
