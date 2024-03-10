5d9ceb4e5dce01ed1b7dce1908e96b0b2b39a2e8
eugenehan94 <eugene.han94@gmail.com>
2024-03-09 20:09:04 -0500

    added navbar for personal info of open-account page

--- src/app/pages/open-account/personal/personal-info/personal-info.module.ts
+++ src/app/pages/open-account/personal/personal-info/personal-info.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonalInfoRoutingModule } from './personal-info-routing.module';
 import { PersonalInfoComponent } from './personal-info.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
-  declarations: [PersonalInfoComponent],
+  declarations: [PersonalInfoComponent, NavbarComponent],
   imports: [CommonModule, PersonalInfoRoutingModule],
 })
 export class PersonalInfoModule {}
