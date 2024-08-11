c463b85c72ef5c8715aa8847d08db2ae221bbf28
eugenehan94 <eugene.han94@gmail.com>
2024-08-10 20:03:43 -0400

    adding breakpoints to transfer-confirmation-dialog component, and adding responsive for desktop

--- src/app/pages/authorized/online-banking/components/transfer-confirmation-dialog/transfer-confirmation-dialog.component.ts
+++ src/app/pages/authorized/online-banking/components/transfer-confirmation-dialog/transfer-confirmation-dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AccountTransferComponent } from '../../pages/account-transfer/account-transfer.component';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+//ngrx
+import { Store } from '@ngrx/store';
+import { setScreenSize } from 'src/app/store/app/actions/app.actions';
 
 @Component({
   selector: 'app-transfer-confirmation-dialog',
@@ -18,11 +23,13 @@ export class TransferConfirmationDialogComponent {
       amount: any;
       fromAccountType: any;
       toAccountType: any;
-    }
+    },
+    private breakpointObserver: BreakpointObserver,
+    private store: Store<any>
   ) {
     console.log('Dialog content: ', this.dialogData);
   }
-
+  currentScreenSize?: string;
   fromAccount? = this.dialogData.fromAccount;
   fromAccountName? = this.dialogData.fromAccountType.AccountType;
   toAccount? = this.dialogData.toAccount;
@@ -33,6 +40,33 @@ export class TransferConfirmationDialogComponent {
     this.dialogData.fromAccountType.Amount - this.dialogData.amount;
   toAccountAmount? =
     this.dialogData.toAccountType.Amount - this.dialogData.amount;
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([
+        '(max-width: 599.99px)',
+        '(min-width: 600px) and (max-width: 1023.99px)',
+        '(min-width: 1024px)',
+      ])
+      .subscribe((result) => {
+        const breakpoints = result.breakpoints;
+        if (this.breakpointObserver.isMatched('(max-width: 599.99px)')) {
+          this.store.dispatch(setScreenSize({ screenSize: 'Small' }));
+        } else if (
+          this.breakpointObserver.isMatched(
+            '(min-width: 600px) and (max-width:1024px)'
+          )
+        ) {
+          this.store.dispatch(setScreenSize({ screenSize: 'Medium' }));
+        } else {
+          this.store.dispatch(setScreenSize({ screenSize: 'Large' }));
+        }
+      });
+
+    this.store.select('screenSizeReducer').subscribe((res) => {
+      this.currentScreenSize = res.currentScreenSize;
+    });
+  }
 
   closeDialog() {
     this.dialogRef.close();
