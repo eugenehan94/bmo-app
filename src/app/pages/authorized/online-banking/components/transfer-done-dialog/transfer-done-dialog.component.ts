8d56f11542b80d91657fd63ca7db2d5022b6f845
eugenehan94 <eugene.han94@gmail.com>
2024-08-23 21:19:45 -0400

    adding breakpoint observer and store using inject with transfer done dialog component

--- src/app/pages/authorized/online-banking/components/transfer-done-dialog/transfer-done-dialog.component.ts
+++ src/app/pages/authorized/online-banking/components/transfer-done-dialog/transfer-done-dialog.component.ts
@@ -1,6 +1,12 @@
 import { Component, inject, model, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TransferConfirmationDialogComponent } from '../transfer-confirmation-dialog/transfer-confirmation-dialog.component';
+
+import { BreakpointObserver } from '@angular/cdk/layout';
+//ngrx
+import { Store } from '@ngrx/store';
+import { setScreenSize } from 'src/app/store/app/actions/app.actions';
+
 @Component({
   selector: 'app-transfer-done-dialog',
   templateUrl: './transfer-done-dialog.component.html',
@@ -10,6 +16,8 @@ import { TransferConfirmationDialogComponent } from '../transfer-confirmation-di
 export class TransferDoneDialogComponent implements OnInit {
   private dialogRef = inject(MatDialogRef<TransferConfirmationDialogComponent>);
   private data = inject(MAT_DIALOG_DATA);
+  private breakpointObserver = inject(BreakpointObserver);
+  private store = inject(Store);
   amount = this.data.amount;
   fromAccount = this.data.fromAccount;
   fromAccountType = this.data.fromAccountType.AccountType;
@@ -17,6 +25,26 @@ export class TransferDoneDialogComponent implements OnInit {
   toAccountType = this.data.toAccountType.AccountType;
   ngOnInit(): void {
     console.log('this.data: ', this.data);
+    this.breakpointObserver
+      .observe([
+        '(max-width: 599.99px)',
+        '(min-width: 600px) and (max-width: 1023.99px)',
+        '(min-width: 1024px)',
+      ])
+      .subscribe((result) => {
+        const breakpoints = result.breakpoints;
+        if (this.breakpointObserver.isMatched('(max-width: 599.99px)')) {
+          this.store.dispatch(setScreenSize({ screenSize: 'Small' }));
+        } else if (
+          this.breakpointObserver.isMatched(
+            '(min-width: 600px) and (max-width:1024px)'
+          )
+        ) {
+          this.store.dispatch(setScreenSize({ screenSize: 'Medium' }));
+        } else {
+          this.store.dispatch(setScreenSize({ screenSize: 'Large' }));
+        }
+      });
   }
   // @Inject(MAT_DIALOG_DATA)
   // public dialogData: {
