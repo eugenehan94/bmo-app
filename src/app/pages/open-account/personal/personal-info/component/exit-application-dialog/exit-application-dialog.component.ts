1128ee913b160fccce86c53b8f954a652510c476
eugenehan94 <eugene.han94@gmail.com>
2024-09-02 13:14:11 -0400

    adding angular material dialog for exit option for open-account personal info page

---
+++ src/app/pages/open-account/personal/personal-info/component/exit-application-dialog/exit-application-dialog.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exit-application-dialog',
+  templateUrl: './exit-application-dialog.component.html',
+  styleUrl: './exit-application-dialog.component.css',
+})
+export class ExitApplicationDialogComponent {}
--- src/app/pages/open-account/personal/personal-info/personal-info.module.ts
+++ src/app/pages/open-account/personal/personal-info/personal-info.module.ts
@@ -3,11 +3,27 @@ import { CommonModule } from '@angular/common';
 import { PersonalInfoRoutingModule } from './personal-info-routing.module';
 import { PersonalInfoComponent } from './personal-info.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { ExitApplicationDialogComponent } from './component/exit-application-dialog/exit-application-dialog.component';
+import { FooterComponent } from './component/footer/footer.component';
 import { DisplayImageComponent } from './component/display-image/display-image.component';
 import { PersonalFormComponent } from './component/personal-form/personal-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialDesignModule } from '../../../../shared/material-design/material-design.module';
 
 @NgModule({
-  declarations: [PersonalInfoComponent, NavbarComponent, DisplayImageComponent, PersonalFormComponent],
-  imports: [CommonModule, PersonalInfoRoutingModule],
+  declarations: [
+    PersonalInfoComponent,
+    NavbarComponent,
+    DisplayImageComponent,
+    PersonalFormComponent,
+    FooterComponent,
+    ExitApplicationDialogComponent,
+  ],
+  imports: [
+    CommonModule,
+    PersonalInfoRoutingModule,
+    ReactiveFormsModule,
+    MaterialDesignModule,
+  ],
 })
 export class PersonalInfoModule {}
