19beae25bb1b1cb83d460df25f7ee1d0d8913803
eugenehan94 <eugene.han94@gmail.com>
2024-09-03 19:20:46 -0400

    completed exit application dialog for open-account

--- src/app/pages/open-account/personal/personal-info/component/exit-application-dialog/exit-application-dialog.component.ts
+++ src/app/pages/open-account/personal/personal-info/component/exit-application-dialog/exit-application-dialog.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { PersonalFormComponent } from '../personal-form/personal-form.component';
 @Component({
   selector: 'app-exit-application-dialog',
   templateUrl: './exit-application-dialog.component.html',
   styleUrl: './exit-application-dialog.component.css',
 })
-export class ExitApplicationDialogComponent {}
+export class ExitApplicationDialogComponent {
+  private dialogRef = inject(MatDialogRef<PersonalFormComponent>);
+  onYesClick() {
+    window.scrollTo(0, 0);
+    this.dialogRef.close('Yes');
+  }
+  onNoClick() {
+    this.dialogRef.close('No');
+  }
+}
