e98fd216d02dbe7b8faef9f08877200e2ad860de
eugenehan94 <eugene.han94@gmail.com>
2024-09-10 20:04:23 -0400

    added error handling for personal-from service

--- src/app/pages/open-account/personal/personal-info/component/personal-form/personal-form.service.ts
+++ src/app/pages/open-account/personal/personal-info/component/personal-form/personal-form.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { catchError, throwError, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
@@ -9,12 +14,27 @@ export class PersonalFormService {
   private createAccountLink =
     'http://localhost:5000/api/v1/open-account/create-account';
 
-  createAccount(data: any) {
+  createAccount(data: any): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
       }),
     };
-    return this.http.post<any>(this.createAccountLink, data, httpOptions);
+    return this.http
+      .post<any>(this.createAccountLink, data, httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An error occurred';
+    if (error.error instanceof ErrorEvent) {
+      // Client side error
+      errorMessage = `Error ${error.error.message}`;
+    } else {
+      // Server side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
   }
 }
